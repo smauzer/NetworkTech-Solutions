b4f1f89fd198d64303fb90a8fd7374fa367a30a5
Nagy Noel <nagynooel@gmail.com>
2023-10-25 20:30:15 +0200

    Szükséges HTML oldalak létrehozva és a hozzájuk tartotó url beállítva.

--- default/urls.py
+++ default/urls.py
@@ -9,5 +9,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_view, name="index")
+    path('', views.index_view, name="index"),
+    path('rolunk', views.about_us_view, name="about_us"),
+    path('kapcsolat', views.contact_view, name="contact"),
+    path('aruhaz', views.shop_view, name="shop"),
+    path('termekek/<str:product_name>', views.product_view, name="product"),
+    path('kosar', views.cart_view, name="cart"),
+    path('fizetes', views.payment_view, name="payment"),
+    path('rendeles/<int:order_number>', views.order_status_view, name="order_status")
 ]
--- default/views.py
+++ default/views.py
@@ -10,3 +10,39 @@ from django.shortcuts import render
 # Index page
 def index_view(request):
     return render(request, "default/index.html")
+
+
+def about_us_view(request):
+    return render(request, "default/rolunk.html")
+
+
+def contact_view(request):
+    return render(request, "default/kapcsolat.html")
+
+
+def shop_view(request):
+    return render(request, "default/aruhaz.html")
+
+
+def product_view(request, product_name):
+    context = {
+        "product_name": product_name
+    }
+    
+    return render(request, "default/termek.html", context=context)
+
+
+def cart_view(request):
+    return render(request, "default/kosar.html")
+
+
+def payment_view(request):
+    return render(request, "default/fizetes.html")
+
+
+def order_status_view(request, order_number):
+    context = {
+        "order_number": order_number
+    }
+
+    return render(request, "default/rendeles-allapota.html", context=context)
