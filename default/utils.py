92c4f5155d69991d721f4ae884b2552eee76f618
Nagy Noel <nagynooel@gmail.com>
2023-11-07 22:55:09 +0100

    Termék objektumok json formátumba lakaítására külön funkció létrehozva.

--- default/utils.py
+++ default/utils.py
@@ -2,25 +2,31 @@ from .models import Product, Category
 
 from django.forms.models import model_to_dict
 
+
+# Format multiple product objects as JSON
+def product_objects_to_json(product_objects, fields):
+    products_json = ""
+    
+    for product in product_objects:
+        products_json += "{"
+        for i, field in enumerate(fields):
+            products_json += f"\"{field}\": \"{product[i]}\","
+        # Remove last colon
+        products_json = products_json[:-1]
+        products_json += "},"
+    
+    products_json = products_json[:-1]
+    products_json = "[" + products_json + "]"
+
+    return products_json
+
+
 # Get all products as JSON
 # Specify the fields needed as the argument
 def get_all_products(fields: list):
     all_product_objects = Product.objects.all().values_list(*fields)
     
-    all_products_json = ""
-    
-    for product in all_product_objects:
-        all_products_json += "{"
-        for i, field in enumerate(fields):
-            all_products_json += f"\"{field}\": \"{product[i]}\","
-        # Remove last colon
-        all_products_json = all_products_json[:-1]
-        all_products_json += "},"
-    
-    all_products_json = all_products_json[:-1]
-    all_products_json = "[" + all_products_json + "]"
-    
-    return all_products_json
+    return product_objects_to_json(all_product_objects, fields)
 
 
 # Get an individual product info as JSON by name
